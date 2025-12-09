fae98c5a3257594dc8c5b39c667a0ca0c7cb0717
Ajayi Dipo <ajayidipo@ymail.com>
2025-12-09 13:36:08 +0100

    update terraform module to fix bugs (#4)
    * update terraform module
    
    * terraform-docs: automated action
    
    * add dns zone name override option
    
    * terraform-docs: automated action
    
    * update data file
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "use_existing_dns_zone" {
   default     = false
 }
 
-variable "dns_zone_name" {
-  description = "DNS zone name"
+variable "dns_name" {
+  description = "DNS name. e.g - test.example.com."
   type        = string
 }
 
@@ -81,6 +81,18 @@ variable "truefoundry_dns_manger_role_id_override_enabled" {
 
 variable "truefoundry_dns_manger_role_id_override_name" {
   description = "Override name for TrueFoundry DNS Manager Role ID"
+  type        = string
+  default     = ""
+}
+
+variable "truefoundry_dns_zone_name_override_enabled" {
+  description = "Enable override for TrueFoundry DNS Zone name"
+  type        = bool
+  default     = false
+}
+
+variable "truefoundry_dns_zone_override_name" {
+  description = "Override name for TrueFoundry DNS Zone"
   type        = string
   default     = ""
 }
